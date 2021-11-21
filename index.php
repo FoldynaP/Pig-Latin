b2ba56c572c4102250c83024b295ad230c4413d9
Petr Foldyna <petr.foldyna12@gmail.com>
2021-11-21 11:42:29 +0100

    Zobrazení přeloženého textu přes session v index.php

--- index.php
+++ index.php
@@ -13,6 +13,16 @@
     <input type="text" name="text" class="input input_text" placeholder="Enter your text">
     <input type="submit" name="submit" class="input input_submit" value="Translate">
 </form>
+<?php
+if(isset($_SESSION["success"]) && $_SESSION["success"] !=''){
+    echo '<h3 class="text-info-success"> '.$_SESSION["success"].' </h3>';
+    unset($_SESSION['success']);
+}
+if(isset($_SESSION["status"]) && $_SESSION["status"] !='') {
+    echo '<h3 class="text-info-fail"> ' . $_SESSION["status"] . ' </h3>';
+    unset($_SESSION['status']);
+}
+?>
 </body>
 </html>
 
