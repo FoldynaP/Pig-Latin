6d031cf050bb007d61421176a4f8235ef6290ac1
Petr Foldyna <petr.foldyna12@gmail.com>
2021-11-21 11:40:05 +0100

    Přidání nadpisu a formuláře v index.php

--- index.php
+++ index.php
@@ -8,7 +8,11 @@
     <title>Pig Latin</title>
 </head>
 <body>
-
+<h1 class="header_text">Hello, this is PigLatin translator</h1>
+<form action="code.php" method="post" class="form">
+    <input type="text" name="text" class="input input_text" placeholder="Enter your text">
+    <input type="submit" name="submit" class="input input_submit" value="Translate">
+</form>
 </body>
 </html>
 
